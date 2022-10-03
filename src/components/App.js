5ce3f509582f475eb7633b647381d61a0a53bb46
William <dc_william@yahoo.com>
2022-10-03 15:58:05 -0400

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from '../notes';
+
+const createNote = (note) => {
+  return <Note {...note}/>
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNote)}
       <Footer />
     </div>
   );
